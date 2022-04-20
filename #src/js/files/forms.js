373d8265f8a3b7d54aa6e324bcd354d514174c16
Ruvick <rudikov.web@ya.ru>
2022-04-20 15:07:02 +0300

    Поправил отправку

--- #src/js/files/forms.js
+++ #src/js/files/forms.js
@@ -531,110 +531,52 @@ if (priceSlider) {
 }
 // ============================================================================================================
 
-// Отправка файла плагином PHPMailer ==========================================================================
-// document.addEventListener('DOMContentLoaded', function () {
-// 	const form = document.getElementById('form');
-// 	form.addEventListener('submit', formSend);
+// Отправка файла плагином PHPMailer 
+document.addEventListener('DOMContentLoaded', function () {
+	let send_btns = document.querySelectorAll("._send_btn")
+	send_btns.forEach(element => {
+		element.onclick = (e) => {
+			e.preventDefault()
+			let formid = element.dataset.formid;
+			let msg = element.dataset.msg;
+			let form = document.getElementById(formid);
 
-// 	async function formSend(e) {
-// 		e.preventDefault();
+			let name = (form.querySelectorAll("input[name=name]").length == 0) ? "" : form.querySelectorAll("input[name=name]")[0].value;
+			let tel = (form.querySelectorAll("input[name=phone]").length == 0) ? "" : form.querySelectorAll("input[name=phone]")[0].value;
+			if (tel == "Телефон*") { form.querySelectorAll("input[name=phone]")[0].classList.add("_error"); return }
 
-// 		let error = formValidate(form);
+			console.log(name)
+			console.log(tel)
 
-// 		let formData = new FormData(form);
-// 		formData.append('image', formImage.files[0]);
+			var params = new URLSearchParams()
 
-// 		if (error === 0) {
-// 			form.classList.add('_sending');
-// 			let response = await fetch('sendmail.php', {
-// 				method: 'POST',
-// 				body: formData
-// 			});
-// 			if (response.ok) {
-// 				let result = await response.json();
-// 				alert(result.message);
-// 				formPreview.innerHTML = '';
-// 				form.reset();
-// 				form.classList.remove('_sending');
-// 			} else {
-// 				alert("Ошибка");
-// 				form.classList.remove('_sending');
-// 			}
-// 		} else {
-// 			alert('Заполните обязательные поля');
-// 		}
+			params.append('name', name)
+			params.append('tel', tel)
+			// params.append('mail', mail)
+			params.append('msg', msg)
 
-// 	}
+			var xhr = new XMLHttpRequest();
 
+			xhr.onload = function (e) {
 
-// 	function formValidate(form) {
-// 		let error = 0;
-// 		let formReq = document.querySelectorAll('._req');
+				if (xhr.status == 200) {
 
-// 		for (let index = 0; index < formReq.length; index++) {
-// 			const input = formReq[index];
-// 			formRemoveError(input);
+					location.href = "/thanks.html"
 
-// 			if (input.classList.contains('_email')) {
-// 				if (emailTest(input)) {
-// 					formAddError(input);
-// 					error++;
-// 				}
-// 			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-// 				formAddError(input);
-// 				error++;
-// 			} else {
-// 				if (input.value === '') {
-// 					formAddError(input);
-// 					error++;
-// 				}
-// 			}
-// 		}
-// 		return error;
-// 	}
-// 	function formAddError(input) {
-// 		input.parentElement.classList.add('_error');
-// 		input.classList.add('_error');
-// 	}
-// 	function formRemoveError(input) {
-// 		input.parentElement.classList.remove('_error');
-// 		input.classList.remove('_error');
-// 	}
-// 	//Функция теста email
-// 	function emailTest(input) {
-// 		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-// 	}
+				} else {
+					console.log(xhr.status)
+					console.log(xhr.statusText)
+					alert("При отправке произошла ошибка")
+				}
 
-// 	//Получаем инпут file в переменную
-// 	const formImage = document.getElementById('formImage');
-// 	//Получаем див для превью в переменную
-// 	const formPreview = document.getElementById('formPreview');
+			}
 
-// 	//Слушаем изменения в инпуте file
-// 	formImage.addEventListener('change', () => {
-// 		uploadFile(formImage.files[0]);
-// 	});
+			xhr.onerror = function (msg) {
+				console.log("eroroa" + xhr.statusText)
+			}
 
-// 	function uploadFile(file) {
-// 		// провераяем тип файла
-// 		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-// 			alert('Разрешены только изображения.');
-// 			formImage.value = '';
-// 			return;
-// 		}
-// 		// проверим размер файла (<2 Мб)
-// 		if (file.size > 2 * 1024 * 1024) {
-// 			alert('Файл должен быть менее 2 МБ.');
-// 			return;
-// 		}
-
-// 		var reader = new FileReader();
-// 		reader.onload = function (e) {
-// 			formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-// 		};
-// 		reader.onerror = function (e) {
-// 			alert('Ошибка');
-// 		};
-// 		reader.readAsDataURL(file);
-// 	}
-// });
+			xhr.open('POST', "http://ips-service.ru/sender.php", true);
+			xhr.send(params);
+		}
+	})
+})
---
+++ #src/sender.php
@@ -0,0 +1,42 @@
+<?
+header("Access-Control-Allow-Origin: *"); 
+// HTTP_ORIGIN
+// REMOTE_ADDR
+// REQUEST_METHOD
+// print_r(json_encode($_SERVER));
+
+if (
+    ($_SERVER["HTTP_ORIGIN"] !== "http://localhost:3000")&&
+    ($_SERVER["HTTP_ORIGIN"] !== "http://стройматериалы-мск.рф")  
+    
+    ) {
+        http_response_code(403);
+        die($_SERVER["HTTP_ORIGIN"]); 
+    }
+
+
+    $to = 'rudikov-web@yandex.ru'; 
+    $subject = 'Обращение с сайта стройматериалы-мск.рф';
+    $message = '
+                <html>
+                    <head>
+                        <title>'.$_REQUEST["msg"].'</title>
+                    </head>
+                    <body>
+                        <p>ФИО: '.$_REQUEST['name'].'</p>
+                        <p>Телефон: '.$_REQUEST['tel'].'</p>                   
+                    </body>
+                </html>'; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers .= "From: Заявка с сайта стройматериалы-мск <noreply@ultrakresla.ru/>\r\n";
+        if (mail($to, $subject, $message, $headers)) {
+            http_response_code(200);
+            die(array());
+        } else {
+            http_response_code(403);
+            die(array());
+        }
+
+
+
+?>
